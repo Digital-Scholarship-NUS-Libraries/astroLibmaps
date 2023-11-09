3877135ff3c5cde84015f11dca8521ca79e06685
gtnbssn <gaetan.boisson@gmail.com>
2023-11-09 11:48:29 +0800

    add google analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 import svelte from "@astrojs/svelte";
-
 import tailwind from "@astrojs/tailwind";
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte(), tailwind()]
+  integrations: [svelte(), tailwind(), partytown()]
 });
