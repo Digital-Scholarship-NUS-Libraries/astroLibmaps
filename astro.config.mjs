02f77ef72db3134daf240075cb199130a0320e7a
gtnbssn <gaetan.boisson@gmail.com>
2023-11-16 17:12:12 +0800

    remove partytown since it breaks view transitions in firefox https://github.com/withastro/astro/issues/8862

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
 
-import partytown from "@astrojs/partytown";
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte(), tailwind(), partytown()]
+  integrations: [svelte(), tailwind()],
 });
+
