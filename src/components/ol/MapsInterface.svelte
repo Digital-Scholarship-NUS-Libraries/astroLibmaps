21c4ad3070347c7b435bcf4c6045a58ad66d68e1
gtnbssn <gaetan.boisson@gmail.com>
2023-11-21 16:02:46 +0800

    Move SVGs outside of the HTML, place page title

--- src/components/ol/MapsInterface.svelte
+++ src/components/ol/MapsInterface.svelte
@@ -41,10 +41,9 @@
     on:click={toggleInterface}
     on:keydown={toggleInterface}
     class="h-12 md:h-8 fill-current"
-    ><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path
-      d="M264.5 5.2c14.9-6.9 32.1-6.9 47 0l218.6 101c8.5 3.9 13.9 12.4 13.9 21.8s-5.4 17.9-13.9 21.8l-218.6 101c-14.9 6.9-32.1 6.9-47 0L45.9 149.8C37.4 145.8 32 137.3 32 128s5.4-17.9 13.9-21.8L264.5 5.2zM476.9 209.6l53.2 24.6c8.5 3.9 13.9 12.4 13.9 21.8s-5.4 17.9-13.9 21.8l-218.6 101c-14.9 6.9-32.1 6.9-47 0L45.9 277.8C37.4 273.8 32 265.3 32 256s5.4-17.9 13.9-21.8l53.2-24.6 152 70.2c23.4 10.8 50.4 10.8 73.8 0l152-70.2zm-152 198.2l152-70.2 53.2 24.6c8.5 3.9 13.9 12.4 13.9 21.8s-5.4 17.9-13.9 21.8l-218.6 101c-14.9 6.9-32.1 6.9-47 0L45.9 405.8C37.4 401.8 32 393.3 32 384s5.4-17.9 13.9-21.8l53.2-24.6 152 70.2c23.4 10.8 50.4 10.8 73.8 0z"
-    /></svg
   >
+    <use xlink:href="/layers.svg#layers" />
+  </svg>
 </div>
 <div
   id="interface"
@@ -59,10 +58,9 @@
     on:click={toggleInterface}
     on:keydown={toggleInterface}
     class="h-12 md:h-8 fill-current"
-    ><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path
-      d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"
-    /></svg
   >
+    <use xlink:href="/xmark.svg#xmark" />
+  </svg>
   <fieldset>
     <legend>Layer swipe</legend>
     <input
