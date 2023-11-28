6a495278ab97ab0b0cf592b94b76aeec0ba3414d
gtnbssn <gaetan.boisson@gmail.com>
2023-11-28 17:17:50 +0800

    put opacity svg inline to avoid repeated http requests

--- src/components/ol/SortableMapItem.svelte
+++ src/components/ol/SortableMapItem.svelte
@@ -125,7 +125,9 @@
       class="h-4 fill-base-content"
     >
       <title>Opacity</title>
-      <use xlink:href="/opacity.svg#opacity" />
+      <path
+        d="M448 256c0-106-86-192-192-192V448c106 0 192-86 192-192zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256z"
+      />
     </svg>
     <input
       type="range"
